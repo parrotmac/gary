aa4dcbcc194782edb5ea1709cbccfb14e88e90fb
Isaac Parker <isaac@linux.com>
2023-03-08 22:36:59 -0700

    default to pg db

--- gary/settings.py
+++ gary/settings.py
@@ -122,8 +122,12 @@ WSGI_APPLICATION = "gary.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 if database_url := os.getenv("DATABASE_URL"):
